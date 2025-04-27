332610b297a1ea063711eeffc13b255613ad74a7
Christian Micieli <149799436+christianmicieli@users.noreply.github.com>
2025-04-27 15:59:36 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,42 +2,77 @@ const sum = require ('./math').sum
 const sub = require ('./math').sub
 const div = require ('./math').div
 const mul = require ('./math').mul
+const pow = require ('./math').pow
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  console.log ("Operazioni disponibili: \n 1.Addizione \n 2.Sottrazione\n 3.Moltiplicazione\n 4.Divisione")
-  
-  readline.question('Inserisci il primo numero: ', num1 => {
-    readline.question('Inserisci il secondo numero: ', num2 => {
+function calc () {
+  console.log ("Operazioni disponibili: \n 0.Per Uscire \n 1.Addizione \n 2.Sottrazione\n 3.Moltiplicazione\n 4.Divisione\n 5.Potenza")
 
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-    readline.question('Che operazione vuoi eseguire?', op => {
+  readline.question('Che operazione vuoi eseguire? ', op =>{
 
-      switch(op){
-
-        case '1':  
-          console.log("Risultato addizione:", sum(num1, num2));
-          break;
-        case '2':
-          console.log("Risultato sottrazione:", sub(num1, num2));
-          break;
-        case '3':
-          console.log("Risultato moltiplicazione:", mul(num1, num2));
-          break;
-        case '4':
-          console.log("Risultato divisione:", div(num1, num2));
-          break;
-          
-        default:
-          console.log('Operazione non valida');
-
-      } 
-
+    if ( op == 0 ){
+      console.log("Grazie per aver usato la nostra calcolatrice <3\n");
       readline.close();
+      return;
+    }
+  
+    readline.question('Inserisci il primo numero: ', num1 => {
+
+      readline.question('Inserisci il secondo numero: ', num2 => {
+
+          num1 = parseFloat(num1);
+          if (isNaN(num1)) {
+            console.log('Errore: il primo numero inserito non è valido.');
+            readline.close();
+            return; 
+          }
+          
+          num2 = parseFloat(num2);
+          if (isNaN(num2)) {
+            console.log('Errore: il secondo numero inserito non è valido.');
+            readline.close();
+            return;  
+          }
+
+
+        switch(op){
+
+          case '1':  
+            console.log("Risultato addizione:", sum(num1, num2));
+            break;
+          case '2':
+            console.log("Risultato sottrazione:", sub(num1, num2));
+            break;
+          case '3':
+            console.log("Risultato moltiplicazione:", mul(num1, num2));
+            break;
+          case '4':
+            if ( num2 == 0 )
+              console.log("Impossibile dividere per zero");
+            else 
+              console.log("Risultato divisione:", div(num1, num2));
+            break;
+          case '5': 
+            console.log("Risultato potenza: ", pow(num1, num2) );
+            break;
+            
+          default:
+            console.log('Operazione non valida');
+
+        } 
+
+        calc();
+
+      });
+
     });
+
   });
-});
+
+}
+
+calc();
